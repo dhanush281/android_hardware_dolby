191d621fc6438bde6c556f9536752983a0941787
Ghosuto <clash.raja10@gmail.com>
2026-07-08 16:42:50 +0000

    dolby: use CLICK insted of DOUBLE_CLICK
    Signed-off-by: pabloescobar-reborn <yogivangala07@gmail.com>

--- dolby/src/org/lunaris/dolby/ui/components/ModernComponents.kt
+++ dolby/src/org/lunaris/dolby/ui/components/ModernComponents.kt
@@ -309,7 +309,7 @@ fun ModernSettingSwitch(
                 checked = checked,
                 onCheckedChange = { 
                     scope.launch {
-                        haptic.performHaptic(HapticFeedbackHelper.HapticIntensity.DOUBLE_CLICK)
+                        haptic.performHaptic(HapticFeedbackHelper.HapticIntensity.CLICK)
                     }
                     onCheckedChange(it)
                 },
@@ -592,7 +592,7 @@ private fun IeqTile(
     Surface(
         onClick = { 
             scope.launch {
-                haptic.performHaptic(HapticFeedbackHelper.HapticIntensity.DOUBLE_CLICK)
+                haptic.performHaptic(HapticFeedbackHelper.HapticIntensity.CLICK)
             }
             onPresetChange(value) 
         },
--- dolby/src/org/lunaris/dolby/ui/screens/ModernEqualizerScreen.kt
+++ dolby/src/org/lunaris/dolby/ui/screens/ModernEqualizerScreen.kt
@@ -625,7 +625,7 @@ private fun ViewModeTile(
     Surface(
         onClick = {
             scope.launch {
-                haptic.performHaptic(HapticFeedbackHelper.HapticIntensity.DOUBLE_CLICK)
+                haptic.performHaptic(HapticFeedbackHelper.HapticIntensity.CLICK)
             }
             onClick()
         },
@@ -743,7 +743,7 @@ private fun BandModeSelector(
                         isSelected = currentMode == mode,
                         onClick = {
                             scope.launch {
-                                haptic.performHaptic(HapticFeedbackHelper.HapticIntensity.DOUBLE_CLICK)
+                                haptic.performHaptic(HapticFeedbackHelper.HapticIntensity.CLICK)
                             }
                             onModeChange(mode)
                         },
